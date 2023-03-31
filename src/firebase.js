e100f7d91e705fa7a68edec205a71908ee9c7d93
eiericksilva <ericksilvaredes@gmail.com>
2023-03-31 17:27:19 -0300

    fix api_key

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: env(API_LEY),
+  apiKey: env(API_KEY),
   authDomain: env(AUTH_DOMAIN),
   projectId: env(PROJECT_ID),
   storageBucket: env(STORAGE_BUCKET),
