5837e9bb57c5345f306f25412af49f3fb99910b8
eiericksilva <ericksilvaredes@gmail.com>
2023-03-31 08:55:21 -0300

    registrando firebase no projeto

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBF0EJ6IoaK10Rpi8NDwscaKDXGVQ--axw",
+  authDomain: "todo-firebase-36932.firebaseapp.com",
+  projectId: "todo-firebase-36932",
+  storageBucket: "todo-firebase-36932.appspot.com",
+  messagingSenderId: "202390877575",
+  appId: "1:202390877575:web:3fd3fc2f0a18b1a05a8f22",
+};
+
+const app = initializeApp(firebaseConfig);
