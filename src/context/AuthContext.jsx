ae8083dead9c4c6a54c5affae261aba7d4344852
eiericksilva <ericksilvaredes@gmail.com>
2023-04-04 14:58:09 -0300

    funcionalidade de login criada

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -11,8 +11,12 @@ const UserContext = createContext();
 
 export const AuthContextProvider = ({ children }) => {
   const [user, setUser] = useState({});
+
   const createUser = (email, password) => {
     return createUserWithEmailAndPassword(auth, email, password);
+  };
+  const signIn = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
   };
   const logout = () => {
     return signOut(auth);
@@ -30,7 +34,7 @@ export const AuthContextProvider = ({ children }) => {
   }, []);
 
   return (
-    <UserContext.Provider value={{ createUser, user, logout }}>
+    <UserContext.Provider value={{ createUser, user, logout, signIn }}>
       {children}
     </UserContext.Provider>
   );
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { UserAuth } from "../../context/AuthContext";
 import {
   Button,
   ColumnOne,
@@ -13,11 +14,25 @@ import {
 } from "./Login";
 
 const Login = () => {
-  const navigate = useNavigate();
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
-    navigate("/todoapp");
+  const { signIn } = UserAuth();
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError("");
+
+    try {
+      await signIn(email, password);
+      navigate("/todoapp");
+    } catch (e) {
+      setError(e.message);
+      console.log(e.message);
+    }
   };
   return (
     <Wrapper>
@@ -35,8 +50,18 @@ const Login = () => {
           <Form onSubmit={handleSubmit}>
             <Title>Login</Title>
             <ContainerInput>
-              <Input type="email" placeholder="E-mail" />
-              <Input type="password" placeholder="Senha" />
+              <Input
+                type="email"
+                placeholder="E-mail"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <Input
+                type="password"
+                placeholder="Senha"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
             </ContainerInput>
             <Button type="submit"> Entrar</Button>
           </Form>
