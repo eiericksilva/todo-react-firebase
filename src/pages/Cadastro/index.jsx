9715e7e8e443144371c45bbae9b85c70ed9c57e9
eiericksilva <ericksilvaredes@gmail.com>
2023-04-03 21:37:28 -0300

    criando o HTML das páginas

--- src/pages/Cadastro/index.jsx
+++ src/pages/Cadastro/index.jsx
@@ -1,7 +1,30 @@
 import React from "react";
 
 const Cadastro = () => {
-  return <div>Cadastro</div>;
+  return (
+    <div>
+      <div>
+        <h2>Página de Cadastro</h2>
+      </div>
+      <div>
+        <form>
+          <label>
+            Email:
+            <input type="email" />
+          </label>
+          <label>
+            Senha:
+            <input type="password" />
+          </label>
+          <label>
+            Confirme sua senha:
+            <input type="password" />
+          </label>
+          <button type="submit"> Entrar</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Cadastro;
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,7 +1,44 @@
 import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Container } from "./Login";
 
 const Login = () => {
-  return <div>Login</div>;
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log("Logado");
+    navigate("todoApp");
+  };
+  return (
+    <Container>
+      <div>
+        <div>
+          <h2>Página de Login</h2>
+          <h4>Entre com sua conta</h4>
+          <p>
+            Ainda não é cadastrado? <Link to="/cadastro">Cadastre-se!</Link>
+          </p>
+        </div>
+        <div>
+          <form onSubmit={handleSubmit}>
+            <label>
+              Email:
+              <input type="email" />
+            </label>
+            <label>
+              Senha:
+              <input type="password" />
+            </label>
+            <button type="submit"> Entrar</button>
+          </form>
+        </div>
+      </div>
+      <div>
+        <h4>Imagem</h4>
+      </div>
+    </Container>
+  );
 };
 
 export default Login;
